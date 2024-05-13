07c5bb263b506f55f818a12fa37c7a110ecac6be
Elie-bou-naoum <eliebounaoum@theplayball.com>
2024-05-13 23:05:51 +0300

    fotsy

---
+++ app/api/turn-on-led/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  const esp2Url = "http://esp2.local/turn-on-led"; // Get DNS of esp2 from environment variable
+
+  try {
+    const response = await axios.get(`${esp2Url}/turn-on-led`); 
+    if (response.status === 200) {
+ 
+      return NextResponse.json(
+        { message: "LED turned on on esp2" },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { error: "Failed to turn on LED on esp2" },
+        { status: 500 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to communicate with ESP2" },
+      { status: 500 }
+    );
+  }
+}
